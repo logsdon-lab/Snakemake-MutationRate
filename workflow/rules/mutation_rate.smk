fef63c415092d4e54738d742e0936dd64166ec7a
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2025-05-14 02:14:55 -0400

    Fix colors. Add ref annotations.

--- workflow/rules/mutation_rate.smk
+++ workflow/rules/mutation_rate.smk
@@ -43,17 +43,29 @@ rule plot_mutation_rate:
     input:
         script="workflow/scripts/plot_mutation_rate.py",
         mut_tsv=rules.calculate_mutation_rate.output,
+        ref_annot_bed=lambda wc: REFERENCES[wc.ref].get("annotations", []),
     output:
         mut_plt=join(OUTPUT_DIR, "mutation_rate", "{ref}", "{rgn}.png"),
     params:
         sample_color=COLORS,
+        sample_shape=SHAPES,
+        ref_annot_bed=lambda wc, input: (
+            f"-r <(grep '{wc.rgn}' {input.ref_annot_bed})"
+            if input.ref_annot_bed
+            else ""
+        ),
     conda:
         "../envs/tools.yaml"
     log:
         join(LOGS_DIR, "plot_mutation_rate_{ref}_{rgn}.log"),
     shell:
         """
-        python {input.script} -i {input.mut_tsv} -o {output} 2> {log}
+        python {input.script} \
+        -i {input.mut_tsv} \
+        -o {output} \
+        -c '{params.sample_color}' \
+        -s '{params.sample_shape}' \
+        {params.ref_annot_bed} 2> {log}
         """
 
 
--- workflow/scripts/plot_mutation_rate.py
+++ workflow/scripts/plot_mutation_rate.py
@@ -1,4 +1,5 @@
 import os
+import ast
 import json
 import argparse
 import matplotlib
@@ -6,10 +7,18 @@ import matplotlib.axes
 import numpy as np
 import polars as pl
 import matplotlib.pyplot as plt
-
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
 from matplotlib.colors import ListedColormap, rgb2hex
 
 IN_COLS = ["ref", "ref_st", "ref_end", "qry", "qry_sm", "qry_sm_divergence_time", "mu"]
+
+
+def minimize_ax(ax: matplotlib.axes.Axes):
+    ax.set_xticks([], [])
+    ax.set_yticks([], [])
+    for side in ["left", "right", "top", "bottom"]:
+        ax.spines[side].set_visible(False)
 
 
 def main():
@@ -20,6 +29,13 @@ def main():
         help=f"BED file of mutation rates. Format: {IN_COLS}",
         required=True,
         type=str,
+    )
+    ap.add_argument(
+        "-r",
+        "--ref_annot",
+        help="Reference annotations as BED file. Does not check mutation rate chrom names. Must limit to range of infile.",
+        default=None,
+        type=argparse.FileType("rb"),
     )
     ap.add_argument(
         "-c",
@@ -93,9 +109,65 @@ def main():
         val: rgb2hex(color)
         for val, color in zip(unique_vals, cmap(np.linspace(0, 1, len(unique_vals))))
     }
-    fig, axes = plt.subplots(figsize=(16, 8), nrows=2, height_ratios=[0.8, 0.2])
-    ax: matplotlib.axes.Axes = axes[0]
-    legend_ax: matplotlib.axes.Axes = axes[1]
+    if args.ref_annot:
+        fig, axes = plt.subplots(
+            figsize=(16, 8), nrows=3, height_ratios=[0.1, 0.8, 0.2], layout="tight"
+        )
+        ref_ax: matplotlib.axes.Axes = axes[0]
+        minimize_ax(ref_ax)
+        ax: matplotlib.axes.Axes = axes[1]
+        legend_ax: matplotlib.axes.Axes = axes[2]
+    else:
+        fig, axes = plt.subplots(
+            figsize=(16, 8), nrows=2, height_ratios=[0.8, 0.2], layout="tight"
+        )
+        ax: matplotlib.axes.Axes = axes[0]
+        legend_ax: matplotlib.axes.Axes = axes[1]
+
+    if args.ref_annot:
+        df_ref_annot = pl.read_csv(
+            args.ref_annot,
+            separator="\t",
+            has_header=False,
+            new_columns=[
+                "chrom",
+                "st",
+                "end",
+                "name",
+                "score",
+                "strand",
+                "thick_st",
+                "thick_end",
+                "item_rgb",
+            ],
+        )
+        patches = []
+        ylim = ref_ax.get_ylim()
+        ylim_ht = ylim[1] - ylim[0]
+        for row in df_ref_annot.iter_rows(named=True):
+            st, end = row["st"], row["end"]
+            try:
+                item_rgb = [
+                    elem / 255 for elem in ast.literal_eval(f"({row['item_rgb']})")
+                ]
+            except Exception:
+                item_rgb = row["item_rgb"]
+
+            patches.append(Rectangle((st, 0), end - st, ylim_ht, color=item_rgb))
+
+        ref_ax.add_collection(PatchCollection(patches, match_original=True))
+
+        # Add border
+        xmin, xmax = df_ref_annot["st"].min(), df_ref_annot["end"].max()
+        rect = Rectangle(
+            (xmin, 0),
+            xmax - xmin,
+            ylim_ht,
+            edgecolor="black",
+            linewidth=1.0,
+            fill=None,
+        )
+        ref_ax.add_patch(rect)
 
     for sm, df_sm in df.group_by(["qry_sm"], maintain_order=True):
         sm = sm[0]
@@ -119,16 +191,16 @@ def main():
 
     handles, labels = ax.get_legend_handles_labels()
 
+    if args.ref_annot:
+        ref_ax.set_xlim(ax.get_xlim())
+
     # Minimize legend axis.
-    legend_ax.set_xticks([], [])
-    legend_ax.set_yticks([], [])
-    for side in ["left", "right", "top", "bottom"]:
-        legend_ax.spines[side].set_visible(False)
+    minimize_ax(legend_ax)
 
     legend_ax.legend(
         frameon=False, loc="center", handles=handles, labels=labels, ncols=10
     )
-    fig.savefig(outplot, bbox_inches="tight")
+    fig.savefig(outplot, bbox_inches="tight", dpi=600)
 
 
 if __name__ == "__main__":
